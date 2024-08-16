8b42ec5357e74f726bb14541f0a7abc8d48f35fa
Eugénie <eugenie.b24@hotmail.fr>
2024-08-16 20:50:26 +0100

    Add ticketReducer logic

--- src/reducers/ticketReducer.js
+++ src/reducers/ticketReducer.js
@@ -1,7 +1,22 @@
 export default function ticketReducer(state, action) {
+  /*Reducer Logic*/
   switch (action.type) {
     case "ADD_TICKET":
-      return {};
+      return { ...state, ticket: [...state.ticket, action.payload] };
+    case "UPDATE_TICKET":
+      return {
+        ...state,
+        tickets: state.ticket.map((ticket) =>
+          ticket.id === action.payload.id ? action.payload : ticket
+        ),
+      };
+    case "DELETE_TICKET":
+      return {
+        ...state,
+        tickets: state.ticket.filter(
+          (ticket) => ticket.id !== action.payload.id
+        ),
+      };
     default:
       return state;
   }
